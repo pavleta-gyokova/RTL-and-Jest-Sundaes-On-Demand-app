74a9aa78ba294092033cd1acdb83f73ef1399f1a
Pavleta Guokowa <pavleta.guokowa@gmail.com>
2022-08-08 18:18:10 +0300

    code and test topping options element

--- src/pages/entry/tests/Optiopns.test.jsx
+++ src/pages/entry/tests/Optiopns.test.jsx
@@ -12,3 +12,20 @@ test("displays image for each scoop from the server", async () => {
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+test("displays image for each topping from the server", async () => {
+  render(<Options optionType="toppings" />);
+
+  //find images
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  //confirm alt text of images
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
